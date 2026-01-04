ac713e9ec73b72e24729e800f3e7d22d3994c4f3
root <root@truenas.local>
2026-01-04 10:17:22 -0800

    fix: config scripts

--- plugins/debug/debug.plugin.sh
+++ plugins/debug/debug.plugin.sh
@@ -1,6 +1,10 @@
 
 _omb_module_require_lib jez_lib
 
+>&2 echo 'INFO: Show oh-my-bash-mrjk debug loaded'
+>&2 echo "INFO:   plugins: ${plugins[@]}"
+>&2 echo "INFO:   aliases: ${aliases[@]}"
+>&2 echo "INFO:   completions: ${completions[@]}"
+>&2 echo "INFO: Create a ~/.config/oh-my-bash/loader.sh to dismiss"
 
-echo "OH-MY-BASH loaded"
 
