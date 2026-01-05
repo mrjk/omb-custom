bb7427a4b2747cf411b9c59e47947b22e46a0fe8
root <root@truenas.local>
2026-01-04 20:46:03 -0800

    fix: default setup

--- config/loader.sh
+++ config/loader.sh
@@ -1,17 +1,48 @@
 
 #>&2 echo 'INFO: Default oh-my-bash-mrjk loader executed'
 
+# This represent a sane but full configuration for a developper
+
 plugins=(
+  # Core
   # battery
   colored-man-pages
 
+  # MrJK
   home_paths
   mise
   direnv
 
-  # optional tools
-  fzf
-  jump
+  # Optional tools
+  # fzf
+  # jump
 )
-aliases=()
-completions=()
+aliases=(
+  # Core
+  general
+  chmod
+  ls
+  misc
+
+  # Extra
+  mrjk_user
+  mrjk_user_dev
+)
+
+if [ $(id -u) = 0 ]; then
+  aliases+=(mrjk_root)
+fi
+
+completions=(
+  # Core
+  brew
+  defaults
+  docker
+  git
+  # jump
+  makefile
+  pip
+  pip3
+  ssh
+  docker-compose
+)
